84fd7a87e59e9cd9835c9dd4e35aa5d7ff3cbbb8
tekadmin <tekadmin@163.com>
2015-07-20 07:41:16 +0000

    project init

---
+++ KindEditorAsset.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Description of KindEditorAsset
+ *  KindEditor资源组织文件
+ * @author Rootkit <Rootkit, 290147164@qq.com>
+ * @link #
+ * @QQ 290147164
+ * @date 2015-3-4
+
+ */
+namespace mkui\kindeditor;
+use yii\web\AssetBundle;
+class KindEditorAsset extends AssetBundle {
+    //put your code here
+    public $js=[
+        'kindeditor-min.js',
+        'lang/zh_cn.js',
+       // 'kindeditor.js'
+    ];
+     public $css=[
+        'themes/default/default.css'
+    ];
+    public $jsOptions=[
+        'charset'=>'utf8',
+    ];
+
+
+    public function init() {
+        //资源所在目录
+        $this->sourcePath = dirname(__FILE__) . DIRECTORY_SEPARATOR ;
+    }
+}
+
+?>
